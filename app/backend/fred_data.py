72975a845a7d20bfb69de16ad90c56c28ab1dde5
Parker Hayashi <parkerhayashi@mac.lan>
2025-05-25 15:22:42 -0400

    Add FredAPI key placeholder

--- app/backend/fred_data.py
+++ app/backend/fred_data.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import plotly.express as px
 
-fred = Fred(api_key='83f734ea6e1e9b6f29d660cdc72c19c4')
+fred = Fred(api_key='INSERT API KEY HERE')
 
 # Get FRED Data
 
